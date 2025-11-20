9b8c38c82e5084c573ffbdc1d3131c58c8f299be
Luan Sampaio <luansampaio0294@gmail.com>
2025-11-20 19:18:51 -0300

    chore: Iniciando setup do projeto com Spring Boot

---
+++ src/main/java/com/cinereviewapp/cinereview_api/CinereviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinereviewapp.cinereview_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinereviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinereviewApiApplication.class, args);
+	}
+
+}
