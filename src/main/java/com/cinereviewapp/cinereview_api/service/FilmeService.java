a8cb478dd4c4c89e6d8a659045cb6a804e8a9443
Luan Sampaio <luansampaio0294@gmail.com>
2025-11-21 16:50:39 -0300

    feat(service): Cria classe FilmeService

---
+++ src/main/java/com/cinereviewapp/cinereview_api/service/FilmeService.java
@@ -0,0 +1,39 @@
+package com.cinereviewapp.cinereview_api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cinereviewapp.cinereview_api.model.Filme;
+
+@Service
+public class FilmeService {
+    public List<Filme> filmes = new ArrayList<>();
+
+    // Retornar todos os filmes
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    // Adicionar filme
+    public void addFilme(Filme filme) {
+        filmes.add(filme);
+    }
+
+    // Filtrar por id
+    public Filme getFilmePorId(Long id) {
+        return filmes.stream()
+        .filter(filme -> filme.getId().equals(id))
+        .findFirst()
+        .orElse(null);
+    }
+
+    // Filtrar por nome de filme
+    public Filme getFilmePorNome(String titulo) {
+        return filmes.stream()
+        .filter(filme -> filme.getTitulo().equals(titulo))
+        .findFirst()
+        .orElse(null);
+    }
+}
